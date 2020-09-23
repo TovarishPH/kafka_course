1e09618f3d750bf09db9efd0f7a8d3e129cb80bd
Paulo Henrique Correa dos Santos <paulo.santos@engdb.com.br>
2020-09-23 14:13:55 -0300

    Java Consumer inside Consumer Group

---
+++ src/main/java/com/github/ph/kafka/tutorial1/ConsumerDemoGroup.java
@@ -0,0 +1,58 @@
+package com.github.ph.kafka.tutorial1;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class ConsumerDemoGroup {
+
+    public static void main(String[] args) {
+
+        /*
+        Esta classe serve apenas como teste, sendo apenas uma cópia da classe 'ConsumerDemo'
+        e tendo apenas o groupId modificado para testes de ConsumerGroups
+         */
+
+        Logger logger = LoggerFactory.getLogger(ConsumerDemoGroup.class.getName());
+
+        String bootstrapServer = "127.0.0.1:9092";
+        String groupId = "my-fifth-application";
+        String topic = "first_topic";
+
+        //create consumer configs
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        //create consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        //subscribe consumer to our topic(s)
+        //para se inscrever em apenas um topic
+        /*consumer.subscribe(Collections.singleton(topic));*/
+        //para se inscrever em multiplos topics
+        consumer.subscribe(Arrays.asList(topic));
+
+        //poll for new data
+        //apenas para teste
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+
+            for (ConsumerRecord<String, String> record : records) {
+                logger.info("Key:" + record.key() + ", Value: " + record.value());
+                logger.info("Partition: " + record.partition() + ", Offset: " + record.offset());
+            }
+        }
+    }
+}
